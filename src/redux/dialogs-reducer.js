6278a1dc44e04d7f7159cfde77c1cf23c4153b5d
Vladislav <v_l_a_d_i_k1@mail.ru>
2020-10-12 12:45:10 +0300

    Add Redux

---
+++ src/redux/dialogs-reducer.js
@@ -0,0 +1,50 @@
+import {
+    SEND_MESSAGE,
+    UPDATE_NEW_MESSAGE_CHANGE
+} from './types';
+
+const initialState = {
+    messages: [
+        {id: 1, message: 'Привет, хорошо', myMessage: false},
+        {id: 2, message: 'Привет, как дела?', myMessage: true},
+        {id: 3, message: 'Чем занимаешься?', myMessage: false},
+        {id: 4, message: 'Ура!', myMessage: true},
+        {id: 5, message: 'Хорошо!', myMessage: false},
+        {id: 6, message: 'Привет, хорошо', myMessage: false},
+        {id: 7, message: 'Привет, как дела?', myMessage: true},
+        {id: 8, message: 'Чем занимаешься?', myMessage: false},
+        {id: 9, message: 'Ура!', myMessage: true},
+        {id: 10, message: 'Хорошо!', myMessage: false},
+    ],
+    newMessageText: '',
+    dialogs: [
+        {id: 1, name: 'Nastya'},
+        {id: 2, name: 'Vladislav'},
+        {id: 3, name: 'Yana'},
+        {id: 4, name: 'Ira'},
+        {id: 5, name: 'Alena'},
+    ]
+}
+
+function dialogsReducer(state = initialState, action) {
+    switch (action.type) {
+        case SEND_MESSAGE:
+            const newMessage = {
+                id: state.messages.length + 1,
+                message: state.newMessageText,
+                myMessage: true
+            }
+            state.messages.push(newMessage)
+            state.newMessageText = ''
+            return state
+
+        case UPDATE_NEW_MESSAGE_CHANGE:
+            state.newMessageText = action.data
+            return state
+
+        default:
+            return state
+    }
+}
+
+export default dialogsReducer
