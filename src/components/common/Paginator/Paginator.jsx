c4f2a6ee1c44c53efbe6033d3785ac8a5575a956
Vladislav <v_l_a_d_i_k1@mail.ru>
2020-11-09 11:39:26 +0300

    Deploy GH page

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import cn from 'classnames'
 import s from './Paginator.module.css';
 import rightArrowImage from '../../../assets/images/nextPage.png'
 import leftArrowImage from '../../../assets/images/prevPage.png'
@@ -25,14 +26,13 @@ const Paginator = ({currentPage, onPageChanged, totalValueCount, pageSize, porti
         <div className={s.pagination}>
             { portionNumber > 1 &&
             <span className={s.button} onClick={onPrevPortionPage}>
-                <img className={s.arrow} src={leftArrowImage} alt=""/>
+                <img className={s.arrow} src={leftArrowImage} alt="leftArrow"/>
             </span>}
             <ul className={s.page_list}>
                 {pages
                     .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
                     .map(item => {
-                    const selected = (item === currentPage) ? s.selected_page : ''
-                    return <li className={s.page_li + ' ' + selected}
+                    return <li className={cn({[s.selected_page]: item === currentPage}, s.page_li)}
                                key={item}
                                onClick={() => onPageChanged(item)}
                     >{item}</li>
@@ -40,7 +40,7 @@ const Paginator = ({currentPage, onPageChanged, totalValueCount, pageSize, porti
             </ul>
             { portionNumber < portionCount &&
             <span className={s.button} onClick={onNextPortionPage}>
-                <img className={s.arrow} src={rightArrowImage} alt=""/>
+                <img className={s.arrow} src={rightArrowImage} alt="rightArrow"/>
             </span>}
         </div>
     )
